ca7d9324f8e256b53457bd7168bd26a226d4dc1b
udaykiran202 <uday43446@gmail.com>
2026-08-27 22:28:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const ctof=document.getElementById("ctof");
+const ftoc=document.getElementById("ftoc");
+const temp=document.getElementById("temps");
+const h3=document.getElementById("H3");
+let res;
+ctof.onclick=function(){
+    let vari=Number(temp.value);
+    res=((vari*9)/5)+32;
+    h3.textContent=`The ${vari} celsius is converted to  ${res} fahrenheit`;
+}
+ftoc.onclick=function(){
+    let vari=Number(temp.value);
+    res=((vari-32)*5)/9;
+    hh3.textContent=`The ${vari} fahrenheit is converted to  ${res} celsius`;
+}
